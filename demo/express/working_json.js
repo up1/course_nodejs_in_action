c7036c42d2954124b928683b7feb80e36a403c2d
somkiat <somkiat.p@gmail.com>
2017-01-27 11:13:53 +0700

    Working with JSON data

---
+++ demo/express/working_json.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+var photos = [];
+photos.push({
+  name: "First",
+  path: "image01.jpg"
+});
+photos.push({
+  name: "SEcond",
+  path: "image02.jpg"
+});
+
+app.get('/photos', function(request, response){
+  response.json(photos)
+});
+
+
+app.listen(3000);
+console.log('Listening on port 3000');
