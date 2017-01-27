b8f578a142788c2ca69b3c1256d95efd11c20130
somkiat <somkiat.p@gmail.com>
2017-01-27 08:44:57 +0700

    Demo of http server

---
+++ demo/web/hello_server_02.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var server = http.createServer();
+server.on('request', function(request, response){
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.end('Hello World\n');
+});
+server.listen(3000);
+console.log('Server running at http://localhost:3000/');
