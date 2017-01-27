5109b293844d9093ab10f59132834d87cf980e35
somkiat <somkiat.p@gmail.com>
2017-01-27 21:27:04 +0700

    Example of module

---
+++ demo/module/calculator.js
@@ -0,0 +1,10 @@
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function minus(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+module.exports.add = add;
+module.exports.minus = minus;
---
+++ demo/module/use.js
@@ -0,0 +1,4 @@
+var calculator = require('./calculator.js');
+
+console.log(calculator.add(1, 2));
+console.log(calculator.minus(1, 2));
