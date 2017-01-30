1c7338f920f660ec4d837b420c6ddc25fcee19e3
somkiat <somkiat.p@gmail.com>
2017-01-30 13:40:04 +0700

    Demo function

--- workshop/day1/demo_function.js
+++ workshop/day1/demo_function.js
@@ -1,5 +1,14 @@
-function add(num1, num2) {
+function add(num1) {
+    return add2(num1, num1);
+}
+
+var add2 = function(num1, num2) {
     return num1 + num2;
 }
 
-console.log(add(1, 2));
+console.log(add2(2, 3));
+console.log(add(1));
+
+var add3 = (num1, num2) => {
+    return num1 + num2;
+}
