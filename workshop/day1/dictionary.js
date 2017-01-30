286142d3eede57b2d901f079ae4a78f9312aca77
somkiat <somkiat.p@gmail.com>
2017-01-30 13:22:03 +0700

    Dictionary

--- workshop/day1/dictionary.js
+++ workshop/day1/dictionary.js
@@ -2,3 +2,18 @@ var user = { "id" : 1234, "name" : "somkiat", "age": 30 }
 console.log( user.id )
 console.log( user.name )
 console.log( user.age )
+
+var users = [  
+    { "id" : 1, "name" : "somkiat 01", "age": 30 },
+    { "id" : 2,  "age": 30 }
+]
+
+for (var i = 0; i < users.length; i++) {
+    if( users[i].name == undefined ) {
+        console.log( users[i].id )
+    } else {
+        console.log( users[i].id + ", " + users[i].name )
+    }
+};
+
+
