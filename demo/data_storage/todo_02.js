5b844938193317a79edf53c6292b2ab2430e1a89
somkiat <somkiat.p@gmail.com>
2017-01-31 21:23:19 +0700

    Working with god file

--- demo/data_storage/todo_02.js
+++ demo/data_storage/todo_02.js
@@ -6,6 +6,30 @@ var newTask = args.join(' ')
 
 const DB_FILE_LOCATION = 'demo.txt'
 
+var readDataFromFile = (callback) => {
+  fs.exists(DB_FILE_LOCATION, (exists) => {
+    var tasks = []
+    if(exists) {
+      fs.readFile(DB_FILE_LOCATION, 'utf8', (error, data) => {
+        if(error) throw error
+        var task = data.toString()
+        var tasks = JSON.parse(task || '[]')
+        callback(tasks)
+      })
+    } else {
+      callback([])
+    }
+  })
+}
+
+var listTasks = () => {
+  readDataFromFile((tasks) => {
+    for (var index in tasks) {
+      console.log(tasks[index])
+    }
+  })
+}
+
 var writeDataToFile = (tasks) => {
   fs.writeFile(DB_FILE_LOCATION, JSON.stringify(tasks), 'utf8', (error) => {
     if(error) throw error
