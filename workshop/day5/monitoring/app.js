38142922a7c024d15fb026139a20faa412e9d4f9
somkiat <somkiat.p@gmail.com>
2017-02-06 01:37:14 +0700

    Create api/memory

--- workshop/day5/monitoring/app.js
+++ workshop/day5/monitoring/app.js
@@ -1,6 +1,22 @@
+var os = require('os');
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
+
+app.get('/api/memory/', function(request, response) {
+    var timestamp = new Date();
+    var free = os.freemem();
+    var total = os.totalmem();
+    var used = total - free;
+
+    response.json([{
+        'percent': parseFloat(((used * 100) / total).toFixed(1)),
+        'usage': used,
+        'total': total,
+        'time': timestamp.getTime(),
+        'id': 'RAM'
+    }]);
+});
 
 app.listen(8080);
 console.log('Listening on port 8080');
