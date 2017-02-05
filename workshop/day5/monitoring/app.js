cee7014f4c87391e23421cc4fae1fcb0590d7cfb
somkiat <somkiat.p@gmail.com>
2017-02-06 02:17:53 +0700

    Final of workshop

--- workshop/day5/monitoring/app.js
+++ workshop/day5/monitoring/app.js
@@ -1,4 +1,5 @@
 var os = require('os');
+var underscore = require('underscore');
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname));
@@ -8,7 +9,6 @@ app.get('/api/memory/', function(request, response) {
     var free = os.freemem();
     var total = os.totalmem();
     var used = total - free;
-
     response.json([{
         'percent': parseFloat(((used * 100) / total).toFixed(1)),
         'usage': used,
@@ -16,6 +16,36 @@ app.get('/api/memory/', function(request, response) {
         'time': timestamp.getTime(),
         'id': 'RAM'
     }]);
+});
+
+var sum = function(p, n) {
+    return p + n;
+}
+
+var get_cpu_percentages = function(time) {
+    var cpus = os.cpus();
+    var timestamp = time || new Date();
+
+    // Just get idle vs non-idle
+    var cpu_percentages = underscore.map(cpus, function(cpu, key) {
+        var values = underscore.values(cpu.times);
+        var total = underscore.reduce(values, sum, 0);
+        var idle = cpu.times.idle;
+
+        return {
+            'percent': parseFloat((((total - idle) * 100) / total).toFixed(1)),
+            'usage': (total - idle),
+            'total': total,
+            'time': timestamp.getTime(),
+            'id': key
+        }
+    });
+
+    return cpu_percentages;
+};
+
+app.get('/api/cpu/', function(request, response) {
+    response.json(get_cpu_percentages());
 });
 
 app.listen(8080);
